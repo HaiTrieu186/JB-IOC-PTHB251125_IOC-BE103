2a061f45513b8b888bfda2f4748d91d41495cc7f
HaiTrieu186 <pnhtrieu186@gmail.com>
2026-03-25 20:36:12 +0700

    Hoan thanh BT2 (Co ban)

---
+++ Session01/BT2-CoBan/src/main/java/com/example/productmanagement/Bt2CoBanApplication.java
@@ -0,0 +1,13 @@
+package com.example.productmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt2CoBanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bt2CoBanApplication.class, args);
+    }
+
+}
