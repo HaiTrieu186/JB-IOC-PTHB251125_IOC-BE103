8e1dde973694a6ea5b2327ee9b2ce287c5b871e6
HaiTrieu186 <pnhtrieu186@gmail.com>
2026-03-27 10:14:44 +0700

    Hoan thanh BT3 (Gioi) Session02

--- Session02/BT2-6/src/main/java/com/example/taskmanagement/controller/TaskController.java
+++ Session02/BT2-6/src/main/java/com/example/taskmanagement/controller/TaskController.java
@@ -1,12 +1,13 @@
 package com.example.taskmanagement.controller;
 
 import com.example.taskmanagement.model.Task;
-import com.example.taskmanagement.model.User;
 import com.example.taskmanagement.service.ITaskService;
-import com.example.taskmanagement.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +19,16 @@ public class TaskController {
     private final ITaskService taskService;
 
     @GetMapping
-    public List<Task> findAllUsers(){
-        return taskService.findAllTasks();
+    public ResponseEntity<?> findAllTasks(@RequestParam(required = false) String title){
+        List<Task> list =taskService.findAllTasks();
+
+        if (title!=null){
+            list= list.stream().filter(
+               t -> t.getTitle().toLowerCase().contains(title.toLowerCase())
+            ).toList();
+        }
+
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
 }
--- Session02/BT2-6/src/main/java/com/example/taskmanagement/controller/UserController.java
+++ Session02/BT2-6/src/main/java/com/example/taskmanagement/controller/UserController.java
@@ -3,8 +3,11 @@ package com.example.taskmanagement.controller;
 import com.example.taskmanagement.model.User;
 import com.example.taskmanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +19,16 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<User> findAllUsers(){
-        return userService.findAllUsers();
+    public ResponseEntity<?> findAllUsers(@RequestParam(required = false) String search){
+       List<User> list= userService.findAllUsers();
+
+        if (search!=null){
+            list= list.stream().filter(
+                    t -> t.getUsername().toLowerCase().contains(search.toLowerCase())
+            ).toList();
+        }
+
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
 }
