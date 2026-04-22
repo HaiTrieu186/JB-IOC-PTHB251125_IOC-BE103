b50bf0841db9c6f7b70b1c527fd9b160cd811b28
HaiTrieu186 <pnhtrieu186@gmail.com>
2026-04-22 21:12:08 +0700

    Hoàn thành BT4 (Gioi) Session13

---
+++ Session13/BT1-6/src/main/java/re/edu/bt16/security/UserDetailServiceCustom.java
@@ -0,0 +1,31 @@
+package re.edu.bt16.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import re.edu.bt16.entity.User;
+import re.edu.bt16.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceCustom implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(username)
+        );
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole()));
+        return new UserPrincipal(user, authorities);
+    }
+}
